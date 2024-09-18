7abeeb6be0addef6f657fdb1b5effea4c38ba737
Javier Hernández Sánchez <javier.hernandezsanchez@outlook.com>
2024-09-18 23:27:36 +0200

    style(login): use tailwind styles for login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './index.css'
 import { Link, useNavigate } from 'react-router-dom'
 import useCredentialStore from '../../state'
 
@@ -22,34 +21,48 @@ const Login = () => {
 	}
 
 	return (
-		<div className='app-container'>
-			<div className='login-card'>
-				<h2>Login</h2>
+		<div className='flex justify-center items-center h-screen w-screen bg-gray-800'>
+			<div className='bg-white p-6 rounded-lg shadow-lg w-80 text-center'>
+				<h2 className='text-2xl mb-6'>Login</h2>
 				<form onSubmit={handleSubmit}>
-					<div className='form-group'>
-						<label htmlFor='username'>Username</label>
+					<div className='mb-4'>
+						<label htmlFor='username' className='block mb-2'>
+							Username
+						</label>
 						<input
 							type='text'
 							id='username'
 							name='username'
 							value={username}
 							onChange={(ev) => setUsernameState(ev.target.value)}
+							className='w-full p-2 border border-gray-300 rounded'
 						/>
 					</div>
-					<div className='form-group'>
-						<label htmlFor='password'>Password</label>
+					<div className='mb-4'>
+						<label htmlFor='password' className='block mb-2'>
+							Password
+						</label>
 						<input
 							type='password'
 							id='password'
 							name='password'
 							value={password}
 							onChange={(ev) => setPasswordState(ev.target.value)}
+							className='w-full p-2 border border-gray-300 rounded'
 						/>
 					</div>
-					<button type='submit'>Login</button>
+					<button
+						className='bg-blue-800 hover:bg-blue-600 text-white py-2 rounded w-full'
+						type='submit'
+					>
+						Login
+					</button>
 				</form>
-				<p>
-					Don't have an account? <Link to={'/signup'}>Sign up</Link>
+				<p className='pt-4'>
+					Don't have an account?{' '}
+					<Link to={'/signup'} className='text-blue-800'>
+						Sign up
+					</Link>
 				</p>
 			</div>
 		</div>
