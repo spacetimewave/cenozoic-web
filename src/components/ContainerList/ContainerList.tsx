788ae58188d7ba841ddf64832670f69c45141009
Javier Hernández Sánchez <javierhersan1999@gmail.com>
2024-10-21 17:35:53 +0200

    feat(container): disable everything when modal is opened

--- src/components/ContainerList/ContainerList.tsx
+++ src/components/ContainerList/ContainerList.tsx
@@ -21,7 +21,7 @@ const Modal = ({
 	message: string
 	onClose: () => void
 }) => (
-	<div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+	<div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
 		<div className='bg-white p-4 rounded shadow-lg'>
 			<p className='text-black'>{message}</p>
 			<button
